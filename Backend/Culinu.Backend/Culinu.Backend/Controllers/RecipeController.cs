8d779b53813fa4d323fd5d0652c911488423fd1e
Sandro <sandro.sembeni@gmail.com>
2022-08-15 17:04:23 +0200

    Add RecipeController Logic

--- Backend/Culinu.Backend/Culinu.Backend/Controllers/RecipeController.cs
+++ Backend/Culinu.Backend/Culinu.Backend/Controllers/RecipeController.cs
@@ -1,3 +1,4 @@
+using Culinu.Backend.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Culinu.Backend.Controllers
@@ -7,16 +8,43 @@ namespace Culinu.Backend.Controllers
     public class RecipeController : ControllerBase
     {
         private readonly ILogger<RecipeController> _logger;
+        private readonly CulinuContext _culinuContext;
 
-        public RecipeController(ILogger<RecipeController> logger)
+        public RecipeController(CulinuContext culinuContext
+            , ILogger<RecipeController> logger)
         {
             _logger = logger;
+            _culinuContext = culinuContext;
         }
 
-        [HttpGet(Name = "GetRecipe")]
-        public int Get()
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<RecipeModel>>> Get([FromBody] string Name)
         {
-            return 1;
+            if (string.IsNullOrEmpty(Name))
+            {
+                return BadRequest();
+            }
+            var recipes = _culinuContext.Recipes?
+                .Where(x => x.Name.ToLower().Contains(Name.ToLower()))
+                .ToList();
+            await _culinuContext.SaveChangesAsync();
+
+            if (recipes != null)
+                return Ok(recipes);
+
+            return BadRequest();
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<RecipeModel>> Post([FromBody] RecipeModel recipe)
+        {
+            if (string.IsNullOrEmpty(recipe.Name) || recipe.Ingredients == null)
+                return BadRequest();
+
+            await _culinuContext.AddAsync(recipe);
+            await _culinuContext.SaveChangesAsync();
+
+            return Ok(recipe);
         }
     }
 }
--- Backend/Culinu.Backend/Culinu.Backend/Models/IngredientModel.cs
+++ Backend/Culinu.Backend/Culinu.Backend/Models/IngredientModel.cs
@@ -1,8 +1,13 @@
-﻿namespace Culinu.Backend.Models
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Culinu.Backend.Models
 {
     public class IngredientModel
     {
-        public int Id { get; set; }
-        public string Name { get; set; }
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; } = default!;
+        public string? Name { get; set; } = default!;
     }
 }
