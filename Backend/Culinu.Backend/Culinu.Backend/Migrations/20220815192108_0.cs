dc6654ec10d9d0aa3aad82d6c972abf04f6c33f9
Sandro <sandro.sembeni@gmail.com>
2022-08-16 19:34:43 +0200

    Add Migrations

---
+++ Backend/Culinu.Backend/Culinu.Backend/Migrations/20220815192108_0.cs
@@ -0,0 +1,86 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace Culinu.Backend.Migrations
+{
+    public partial class _0 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Recipe",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Name = table.Column<string>(type: "text", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Recipe", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Ingredient",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Name = table.Column<string>(type: "text", nullable: true),
+                    RecipeModelId = table.Column<int>(type: "integer", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Ingredient", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Ingredient_Recipe_RecipeModelId",
+                        column: x => x.RecipeModelId,
+                        principalTable: "Recipe",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateTable(
+                name: "RecipeDescription",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Description = table.Column<string>(type: "text", nullable: true),
+                    RecipeModelId = table.Column<int>(type: "integer", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_RecipeDescription", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_RecipeDescription_Recipe_RecipeModelId",
+                        column: x => x.RecipeModelId,
+                        principalTable: "Recipe",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Ingredient_RecipeModelId",
+                table: "Ingredient",
+                column: "RecipeModelId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_RecipeDescription_RecipeModelId",
+                table: "RecipeDescription",
+                column: "RecipeModelId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Ingredient");
+
+            migrationBuilder.DropTable(
+                name: "RecipeDescription");
+
+            migrationBuilder.DropTable(
+                name: "Recipe");
+        }
+    }
+}
